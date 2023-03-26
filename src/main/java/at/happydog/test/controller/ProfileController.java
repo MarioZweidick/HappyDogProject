b6ffce7c5015e817cc6cf81921c87741718c68c3
Horo <devine.root@gmail.com>
2023-03-26 12:21:51 +0200

    Issue #1: Added backend functionality to store profile pictures as oid's in the database and changed profile.html to add a profile picture to the user.

--- src/main/java/at/happydog/test/controller/ProfileController.java
+++ src/main/java/at/happydog/test/controller/ProfileController.java
@@ -3,11 +3,17 @@ package at.happydog.test.controller;
 import at.happydog.test.enity.AppUser;
 import at.happydog.test.service.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.Optional;
 
 /**
 
@@ -38,12 +44,35 @@ public class ProfileController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         //Erstellt einen temporären AppUser für den eingeloggten User
-        AppUser appUser = appUserService.findAppUserByUsername(auth.getName());
+        AppUser appUser = (AppUser) appUserService.loadUserByUsername(auth.getName());
 
         //Added den AppUser zur Model View um in durch die Template Engine (Thymleaf) aufzurufen
         mav.addObject("appuser", appUser);
 
         return mav;
+    }
+
+    @GetMapping("/image/{id}")
+    public ResponseEntity<byte[]> downloadImage(@PathVariable Long id) {
+        Optional<AppUser> optionalAppUser = Optional.ofNullable(appUserService.findAppUserById(id));
+
+        if(optionalAppUser.isPresent()){
+            byte[] image = appUserService.downloadImageFromAppUser(optionalAppUser.get());
+            return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/jpeg")).body(image);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.valueOf("image/jpeg")).body(null);
+        }
+    }
+
+    @PostMapping("/save-image")
+    public String saveAppUserImage(@ModelAttribute AppUser appUser, @RequestParam("image") MultipartFile multipartFile) throws IOException {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        appUser = (AppUser) appUserService.loadUserByUsername(auth.getName());
+
+        appUserService.addAppUserImage(appUser, multipartFile);
+        return "redirect:/user/profile";
     }
 
 }
